791a6b1e7a4712c5924813c6a3091d30986f3f0c
Emily Fernandez <emilyfern121@gmail.com>
2021-08-16 19:22:35 -0400

    added bootstrap react styling to the employee.js file

--- client/src/Components/Employee/Employee.js
+++ client/src/Components/Employee/Employee.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup'
 
 
 const EmployeeWelcome = () => {
     return (
         <div>
-            <h1>Welcome Employee</h1>
-            <h2>Today's Date</h2>
-            <h2>Location</h2>
+            <ListGroup>
+            <ListGroup.Item>Welcome Employee</ListGroup.Item>
+            <ListGroup.Item>Today's Date</ListGroup.Item>
+            <ListGroup.Item>Location</ListGroup.Item>
+            </ListGroup>
         </div>
         
     )
