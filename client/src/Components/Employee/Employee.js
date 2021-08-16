31ad369f08fba644294672f37a2442f6889ab465
Emily Fernandez <emilyfern121@gmail.com>
2021-08-16 18:51:13 -0400

    added employee welcome component

--- client/src/Components/Employee/Employee.js
+++ client/src/Components/Employee/Employee.js
@@ -1,57 +1,15 @@
 import React from 'react';
-import { List, Typography, Divider } from 'antd';
 
-const data = [
-  'Welcome New Employee!',
-  'Date',
-  'Location',
-];
 
-ReactDOM.render(
-  <>
-    <Divider orientation="left">Default Size</Divider>
-    <List
-      header={<div>Header</div>}
-      footer={<div>Footer</div>}
-      bordered
-      dataSource={data}
-      renderItem={item => (
-        <List.Item>
-          <Typography.Text mark>[ITEM]</Typography.Text> {item}
-        </List.Item>
-      )}
-    />
-    <Divider orientation="left">Small Size</Divider>
-    <List
-      size="small"
-      header={<div>Header</div>}
-      footer={<div>Footer</div>}
-      bordered
-      dataSource={data}
-      renderItem={item => <List.Item>{item}</List.Item>}
-    />
-    <Divider orientation="left">Large Size</Divider>
-    <List
-      size="large"
-      header={<div>Header</div>}
-      footer={<div>Footer</div>}
-      bordered
-      dataSource={data}
-      renderItem={item => <List.Item>{item}</List.Item>}
-    />
-  </>,
-  mountNode,
-);
-
-// const EmployeeWelcome = () => {
-//     return (
-//         <div>
-//             <h1>Welcome Employee</h1>
-//             <h2>Today's Date</h2>
-//             <h2>Location</h2>
-//         </div>
+const EmployeeWelcome = () => {
+    return (
+        <div>
+            <h1>Welcome Employee</h1>
+            <h2>Today's Date</h2>
+            <h2>Location</h2>
+        </div>
         
-//     )
-// }
+    )
+}
 
-// export default EmployeeWelcome;
+export default EmployeeWelcome;
