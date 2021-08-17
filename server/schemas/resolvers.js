4ad487964716e6ab99fb6f9f1d3a12dc0a4d53dd
Elcin Koyuncu <79725177+ElcinKoyuncu@users.noreply.github.com>
2021-08-17 15:20:07 -0500

    adding files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,17 @@
 /**
  * import requirements for resolvers to use
  */
-
+ const { AuthenticationError } = require('apollo-server-express');
+ const { Admin, Time, User  } = require('../models');
+ const { signToken } = require('../utils/auth');
 
 //! input resolver functions: queries, mutations
 const resolvers = {
-
-
+    Query: {
+        getUser: async (parent, args, context) => {
+          console.log("User Information",context.user);
+          return await User.findById(context.user._id);
+        }
 
 
 }; //end of resolvers 
